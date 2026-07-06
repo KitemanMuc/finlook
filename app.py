f278660df7ff0a2772afde714bdf489c8e655ac8
KitemanMuc <kiteman@t-online.de>
2026-07-06 08:25:05 +0000

    yfinance test

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import yfinance as yf
+
+ticker = yf.Ticker("GOOG")
 
 st.title("FinLook")
+hist = ticker.history(period="6mo")
 
 st.write("Hello World")
+
+print(hist.head())
